cbb2299d8ae9aaaa6edcf8fddc4c1fc760932d6e
cylqqqcyl <2540251142@qq.com>
2022-10-16 15:27:07 +0800

    added customized event handler

--- src/admin.py
+++ src/admin.py
@@ -5,6 +5,41 @@ from pyftpdlib.log import LogFormatter
 import logging
 import os
 
+class MyHandler(FTPHandler): # customized event handler
+
+    def on_connect(self):
+        print("%s:%s connected" % (self.remote_ip, self.remote_port))
+
+    def on_disconnect(self):
+        # do something when client disconnects
+        pass
+
+    def on_login(self, username):
+        # do something when user login
+        pass
+
+    def on_logout(self, username):
+        # do something when user logs out
+        pass
+
+    def on_file_sent(self, file):
+        # do something when a file has been sent
+        pass
+
+    def on_file_received(self, file):
+        # do something when a file has been received
+        pass
+
+    def on_incomplete_file_sent(self, file):
+        # do something when a file is partially sent
+        pass
+
+    def on_incomplete_file_received(self, file):
+        # remove partially uploaded files
+        import os
+        os.remove(file)
+
+
 cur_dir = __file__.strip('src\\admin.py')
 user_dir = cur_dir+"\\user_dir"
 
